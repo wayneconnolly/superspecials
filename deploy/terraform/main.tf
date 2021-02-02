2828c78d1805625944a02013a546c3cacc21c6f7
Has Been <hasbeen@thatsbeen.com>
2021-02-03 00:15:08 +1100

    test vstsbuildterraform

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -15,15 +15,15 @@ resource "azurerm_resource_group" "superspecials" {
 
 # Create a Storage Account and enable Static website hosting
 resource "azurerm_storage_account" "superspecials" {
-  name                     = "${var.dns_name}"
-  location                 = "${azurerm_resource_group.superspecials.location}"
-  resource_group_name      = "${azurerm_resource_group.superspecials.name}"
+  name                     = "sscoles"
+  location                 = "Australia Southeast"
+  resource_group_name      = "superspecials"
   account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
    provisioner "local-exec" {
-    command = "az storage blob service-properties update --account-name ${azurerm_storage_account.superspecials.site} --static-website  --index-document index.html --404-document 404.html"
+    command = "az storage blob service-properties update --account-name vstsbuildterraform --static-website  --index-document index.html --404-document 404.html"
   }
 }
 
