772a0fb0d5eedb36d73af6f3c5320c3efa3afb63
Has Been <hasbeen@thatsbeen.com>
2021-02-02 22:39:09 +1100

    changed accountname

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -16,14 +16,14 @@ resource "azurerm_resource_group" "superspecials" {
 # Create a Storage Account and enable Static website hosting
 resource "azurerm_storage_account" "superspecials" {
   name                     = "${var.dns_name}"
-  location                 = "${azurerm_resource_group.demo-rg.location}"
-  resource_group_name      = "${azurerm_resource_group.demo-rg.name}"
+  location                 = "${azurerm_resource_group.superspecials.location}"
+  resource_group_name      = "${azurerm_resource_group.superspecials.name}"
   account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
    provisioner "local-exec" {
-    command = "az storage blob service-properties update --account-name ${azurerm_storage_account.webblob.name} --static-website  --index-document index.html --404-document 404.html"
+    command = "az storage blob service-properties update --account-name ${azurerm_storage_account.superspecials} --static-website  --index-document index.html --404-document 404.html"
   }
 }
 
