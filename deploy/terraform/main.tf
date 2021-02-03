d9d239f570c011fa95b299c1de9ecb0b37698d70
Has Been <hasbeen@thatsbeen.com>
2021-02-03 14:17:11 +1100

    installed new terraform

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -1,24 +1,23 @@
 provider "azurerm" {
-    # The "feature" block is required for AzureRM provider 2.x.
-    # If you're using version 1.x, the "features" block is not allowed.
-    version = "~>2.0"
     features {}
 }
-
+terraform {
+  backend "azurerm" {}
+}
 data "azurerm_client_config" "current" {}
 
 #Create Resource Group
 resource "azurerm_resource_group" "resource_group" {
-  name     = var.resource_group
-  location = var.location
+  name     = "${var.resource_group}"
+  location = "${var.location}"
 }
 
 #Create Storage account
 resource "azurerm_storage_account" "storage_account" {
-  name                = var.storage_account
-  resource_group_name = azurerm_resource_group.resource_group.name
+  name                = "${var.storage_account}"
+  resource_group_name = "${azurerm_resource_group.resource_group.name}"
 
-  location                 = var.location
+  location                 = "${var.location}"
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
@@ -29,9 +28,9 @@ resource "azurerm_storage_account" "storage_account" {
 }
 
 #Add index.html to blob storage
-resource "azurerm_storage_blob" "example" {
+resource "azurerm_storage_blob" "site" {
   name                   = "index.html"
-  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_account_name   = "${azurerm_resource_group.storage_account.name}"
   storage_container_name = "$web"
   type                   = "Block"
   content_type           = "text/html"
--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -1,14 +1,17 @@
 variable "location" {
   type        = string
   description = "Azure Region Location"
+  default = "Australia Southeast"
 }
 
 variable "resource_group" {
   type        = string
   description = "Resource Group Name"
+  default = "superspecials"
 }
 
 variable "storage_account" {
   type        = string
   description = "Storage Account Name"
+  default = "vstsbuildterraform"
 }
